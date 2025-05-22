d5847427cb57cdc92f312ef4707d259a6cf337d6
Aankit Chakrabarti <aankitchakrabarti9000@gmail.com>
2025-05-22 22:03:24 +0530

    Add files via upload
    Day 1 and 2 of my JS journey.

---
+++ Day 2 conditional statements/index.js
@@ -0,0 +1,12 @@
+let age = 5;
+// Ternary operator
+age == 5?(console.log("You are 5")):(console.log("You are not 5"));
+/*
+if(age == 5){
+    console.log("You are 5")
+}
+else{
+    console.log("You are not 5")
+}
+    */
+
