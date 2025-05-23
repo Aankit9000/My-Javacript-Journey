9553456d440571f9c858f117f7d20d66745c0279
Aankit Chakrabarti <aankitchakrabarti9000@gmail.com>
2025-05-23 22:35:24 +0530

    Add files via upload

---
+++ Day 4/script .js
@@ -0,0 +1,10 @@
+// Strings
+let a = "berry";
+console.log(a)
+console.log(a[1])
+
+let my_friend_name = "Dipu";
+let my_name = "Aankit"
+// Template literal
+console.log(`I am ${my_name} and my friend's name is\n${my_friend_name}`)
+console.log(a.toUpperCase());
