10d65d5bb5b5c52a733977707790da91c879ce71
Aankit Chakrabarti <aankitchakrabarti9000@gmail.com>
2025-05-25 00:45:40 +0530

    Add files via upload

---
+++ Day 5 Strings/index.js
@@ -0,0 +1,9 @@
+let a = "Your name";
+
+console.log(a);
+
+console.log(a.toUpperCase());
+console.log(a.slice(1, 8));
+console.log(a.replace("Your","My"));
+console.log(a.charAt(0));
+console.log(a.substring())
