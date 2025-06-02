e4c1e6326eee7f747453bca636bb25f07dd7bf92
Aankit Chakrabarti <aankitchakrabarti9000@gmail.com>
2025-06-02 21:32:00 +0530

    Add files via upload

---
+++ Day 6 Arrays/script.js
@@ -0,0 +1,10 @@
+let arr = [1, 2, 3, 4, 5]
+
+console.log(arr)
+console.log(arr.length)
+console.log(arr[3])
+console.log(arr[0])
+console.log(arr.toString())
+console.log(arr.join(" and "))
+console.log(arr.pop())
+console.log(arr)
