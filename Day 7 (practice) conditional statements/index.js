1a9475609f476ce78dcc7250503ae09646a05c6a
Aankit Chakrabarti <aankitchakrabarti9000@gmail.com>
2025-06-02 22:00:43 +0530

    Add files via upload

---
+++ Day 7 (practice) conditional statements/index.js
@@ -0,0 +1,9 @@
+// ternary operator
+
+let a = 2;
+
+a == 1?(console.log("br")):(console.log("brr"));
+
+let age = 20;
+let message = age >= 18 ? "You are an adult" : "You are a minor";
+console.log(message); 
